1826206c9489ff0978aa5283be3977853f3bef3f
gasolin <gasolin@gmail.com>
2015-03-18 10:18:54 +0800

    init

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
 ![project logo](https://raw.github.com/webapplate/ghapplate/gh-pages/public/style/icons/icon128.png) 
 
-# ghapplate 
+# Taiwan PM2.5 monitor
 
-The template to instantly create and edit a new web app on your github page.
+Browse [the Web App](http://gasolin.github.io/twpm25)
 
 Based on [webapplate](http://webapplate.github.io/).
 
-current version: v2.2.0
-
-## How it looks
-
-Open http://webapplate.github.io/ghapplate to see the basic looks and feel.
-
-![Real welcome page](http://i.imgur.com/8AGwXCG.png)
-
-## How to use ghapplate
-
-Go to https://github.com/webapplate/ghapplate, click 'fork' button to fork the template to your repository.
-
-## How to create multiple forks of a GitHub repo?
-
-Check the [guide](https://adrianshort.org/create-multiple-forks-of-a-github-repo/).
-
-## Features
-
-Learn more from [webapplate document](https://github.com/webapplate/webapplate/wiki).
 
 ## License
 
--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,49 @@
   'use strict';
   document.addEventListener('DocumentLocalized', function() { // l20n ready
     document.body.classList.remove('hidden');
-    $.material.ripples();
+    $(".button-collapse").sideNav();
+    var currentLocation = localStorage.getItem('location');
+    var source;
+    var renderList = function() {
+      var item;
+      var fragment = '';
+      for (var i = 0, len = source.length; i < len; i++) {
+        item = source[i];
+        //console.log(item.place.text + ':' + item.value);
+        fragment += '<li';
+        if (currentLocation === item.place.text) {
+          fragment += 'active';
+            //$('#place').text();
+          $('#pm25').text(item.place.text+': '+item.value);
+          $('#link').attr('href', item.place.href);
+        }
+        fragment += '><a href="#" id="' + item.place.text + '">' + item.place.text + '</a></li>';
+      }
+      $('#nav-mobile').html(fragment);
+    }
+
+    $.ajax({
+      url:"https://www.kimonolabs.com/api/2l1lfojw?apikey=9qOqwqCs7joi6nZ7ALvsqsZsmtd9N1hC",
+      crossDomain: true,
+      dataType: "jsonp",
+      success: function (response) {
+        source = response.results.pm25;
+        renderList();
+      },
+      error: function (xhr, status) {
+        //handle errors
+      }
+    });
+
+    $('#nav-mobile').click(function(evt) {
+      console.log(evt.target.id);
+      //save to localstorage
+      localStorage.setItem('location',evt.target.id);
+      currentLocation = evt.target.id;
+      //JSON.stringify(testObject));
+      renderList();
+      $("#sidenav-overlay").trigger("click");
+    })
     // document.addEventListener('deviceready', this.onDeviceReady, false); // cordova ready
 
     // App.init();
